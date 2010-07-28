8e10d220e219b33499caebbd7d4f2d1c32e73e34
Michael Geary <mg@mg.to>
2010-07-28 07:18:58 +0000

    Fix IE8, with a tip of the hat to excanvas: http://code.google.com/p/explorercanvas/source/detail?r=43&path=/excanvas.js

--- code/polygonzo.js
+++ code/polygonzo.js
@@ -90,7 +90,7 @@ PolyGonzo = {
 				var vml = [], iVml = 0;
 				eachShape( places, zoom, offset, function( offsetX, offsetY, place, shape, coords, nCoords, fillColor, fillOpacity, strokeColor, strokeOpacity, strokeWidth ) {
 					
-					vml[iVml++] = '<pgz_vml_:shape style="position:absolute;width:10;height:10;" coordorigin="';
+					vml[iVml++] = '<pgz_vml_:shape style="position:absolute;width:10px;height:10px;" coordorigin="';
 					vml[iVml++] = -~~( offsetX * 10 - .5 );
 					vml[iVml++] = ' ';
 					vml[iVml++] = -~~( offsetY * 10 - .5 );
@@ -149,7 +149,7 @@ PolyGonzo = {
 		function onetime() {
 			PolyGonzo.msie = !! document.namespaces;
 			if( PolyGonzo.msie  &&  ! document.namespaces.pgz_vml_ ) {
-				document.namespaces.add( 'pgz_vml_', 'urn:schemas-microsoft-com:vml' );
+				document.namespaces.add( 'pgz_vml_', 'urn:schemas-microsoft-com:vml', '#default#VML' );
 				document.createStyleSheet().cssText = 'pgz_vml_\\:*{behavior:url(#default#VML)}';
 			}
 		}
