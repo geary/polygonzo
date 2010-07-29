d7766bf711bf3dcdd45319f2f00291a11116e275
Michael Geary <mg@mg.to>
2010-07-29 17:37:10 +0000

    New API loader for easier API key management on multiple domains

---
+++ code/apiloader-polygonzo.googlecode.com.js
@@ -0,0 +1,27 @@
+// apiloader-polygonzo.googlecode.com.js
+// Copyright 2010 Michael Geary - http://mg.to/
+// Free Beer and Free Speech License. Enjoy!
+// http://freebeerfreespeech.org/
+
+// To run the PolyGonzo code in a different domain, copy this file
+// to a new file with a name like this:
+// apiloader-yourhostname.js
+// apiloader-yourhostname-yourportnumber.js
+// apiloader-www.example.com.js
+// apiloader-www.example.com-exampleportnumber.js
+//
+// Then edit the new file and change the key value below to your
+// Google API key for your host/domain name (and port number
+// if you have one):
+// http://code.google.com/apis/maps/signup.html
+
+(function() {
+	
+	var key = 'ABQIAAAAgNQJhbWKFHRJJiHCXotPZxRJvxoAJQuaWxPltcT3CawrQ4FnGhRYbFhfP2Q0bGOOdNEWSQez90jIXw';
+	
+	document.write(
+		'<script type="text/javascript" src="http://www.google.com/jsapi?key=', key, '">',
+		'<\/script>'
+	);
+
+}());
